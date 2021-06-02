a8e3f23fc31acedd6f7962ebabc2ac5ddee56682
Marina Monsivais <minamonsi@me.com>
2021-06-01 22:02:02 -0500

    Imported TodoList.js into App.js, created input field & buttons

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,24 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from "react";
+
+class TodoList extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            newTask: ''
+        }
+    }
+
+    render(){
+        return(
+            <form>
+                <input type="text" name="newTask" value={this.state.newTask}/>
+                <button>Submit</button>
+                <button>Clear</button>
+            </form>
+        )
+    }
+}
+
+export default TodoList;
